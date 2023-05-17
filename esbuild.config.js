0e9fc2792b0649c4be2406dfee89539661c05832
thinhdn266 <thinhdn266@gmail.com>
2023-05-17 20:00:45 +0700

    using esbuild-node-externals

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,10 +1,12 @@
-const res = require("esbuild").buildSync({
+const { nodeExternalsPlugin } = require("esbuild-node-externals");
+
+require("esbuild").build({
   entryPoints: ["src/app.jsx"],
   bundle: true,
   minify: true,
   format: "cjs",
   sourcemap: true,
   outfile: "dist/output.js",
-  packages: "external",
-  // external: ['react', 'react-dom'],
+  plugins: [nodeExternalsPlugin()],
+  // external: ["react", "react-dom"],
 });
